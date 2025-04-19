421a183c7334161c82c8dcc7da159069c53462e2
jordydutch <jordy@sigmatic.io>
2025-04-19 15:17:58 +0200

    docs: improve grammar in Natspec comments

--- src/ISLYX.sol
+++ src/ISLYX.sol
@@ -2,26 +2,25 @@
 pragma solidity ^0.8.0;
 
 interface ISLYX {
-    /// @notice Calculate how many staked LYX (including accumulated rewards) in the vault can be obtained in exchange for `sLYXAmount`.
+    /// @notice Calculate the amount of sLYX tokens to the equivalent staked LYX (including accumulated rewards) in the linked vault.
     ///
-    /// @dev Determine the value of sLYX in terms of the native token balance (LYX) staked in the vault.
-    /// This function calculates the amount of LYX backing an amount of sLYX.
-    /// Formula: (sLYX amount * total stake held by sLYX contract in Vault) / total sLYX minted.
+    /// @dev This function determines the amount of native tokens (staked LYX) that are backing a given amount of sLYX tokens.
+    /// Formula: (sLYX amount * total LYX stake held by the `SLYXToken` contract address in the vault) / total sLYX minted.
     ///
-    /// @param sLyxAmount The amount of sLYX to convert to staked LYX.
-    /// @return The amount of staked LYX backing a specific sLYX amount (in wei).
+    /// @param sLyxAmount The amount (in wei) of sLYX tokens to convert to staked LYX.
+    /// @return The equivalent amount of staked LYX (in wei) backing the specified sLYX amount.
     function getNativeTokenValue(uint256 sLyxAmount) external view returns (uint256);
 
-    /// @notice Calculate how many sLYX tokens can be obtained when converting `stakedLyxAmount`.
+    /// @notice Calculate how many sLYX tokens can be obtained in exchange for `stakedLyxAmount`.
     ///
     /// @dev Calculate the amount of sLYX backing an amount of LYX.
-    /// Formula: (LYX amount * total sLYX minted) / total stake held by sLYX contract in Vault.
+    /// Formula: (LYX amount * total sLYX minted) / total stake held by the `SLYXToken` contract address in the Vault.
     ///
-    /// @param stakedLyxAmount The amount of staked LYX to calculate if exchanging to mint sLYX tokens.
-    /// @return The amount of sLYX backing a specific amount of staked LYX (in wei).
+    /// @param stakedLyxAmount The amount (in wei) of staked LYX to convert into sLYX tokens.
+    /// @return The equivalent amount of sLYX (in wei) backing a specific amount of staked LYX.
     function getSLYXTokenValue(uint256 stakedLyxAmount) external view returns (uint256);
 
-    /// @dev Get the current sLYX / LYX exchange rate to calculate how many sLYX are backing a certain amount of LYX.
+    /// @dev Get the current sLYX / LYX exchange rate to calculate how many sLYX back a certain amount of LYX.
     /// @return The amount of LYX backing 1 sLYX (in wei).
     function getExchangeRate() external view returns (uint256);
 }
